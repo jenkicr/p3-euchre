abb8ba9e075c61792b0bd89597b09a815c2dd259
BraydenLichty <blichty@umich.edu>
2025-03-06 14:02:34 -0500

    j

---
+++ euchre.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Player.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 12) {
+        cout << "Usage: euchre.exe PACK_FILENAME [shuffle|noshuffle] "
+     << "POINTS_TO_WIN NAME1 TYPE1 NAME2 TYPE2 NAME3 TYPE3 "
+     << "NAME4 TYPE4" << endl;
+        return 1;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
